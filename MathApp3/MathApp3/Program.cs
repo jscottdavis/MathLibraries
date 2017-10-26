13511cb7a1ace0e67e7411cdc9a3660a912c295e
User <User@LEN-LBDKFZ>
2017-10-26 15:18:49 -0400

    Initial check-in

---
+++ MathApp3/MathApp3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2+2 = 4");
+        }
+    }
+}
